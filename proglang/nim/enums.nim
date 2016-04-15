ed3e7ced2ae18c29349ad4145037fbcb17ab1ab3
Pascal Munerot <pascal.munerot@gmail.com>
2016-04-15 02:24:20 +0200

    updated prime functions (buggy)

---
+++ proglang/nim/enums.nim
@@ -0,0 +1,12 @@
+type
+  Directions = enum North, South, East, West {. pure .}
+    ##  Directions
+
+  DirectionsNotPure = enum dnNorth, dnSouth, dnEast, dnWest
+    ## Directions (not pure i.e. global naming)
+
+  Primes = enum ONE=1, THREE=3, FIVE=5, SEVEN=7, ELEVEN=11, THIRTEEN=13, TWENTY_ONE=21
+    ## First primes as an enumeration
+
+
+
