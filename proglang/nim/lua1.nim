32da871e1f89327833cc89c1ffaeb57a6cffc1dd
Pascal Munerot <pascal.munerot@gmail.com>
2016-06-08 01:57:17 +0200

    updated with a sample activity diagram

--- proglang/nim/lua1.nim
+++ proglang/nim/lua1.nim
@@ -5,6 +5,14 @@ proc abc(a, b: int): int =
 
 var L = newNimLua()
 L.bindFunction(abc)
+
 L.bindFunction:
   abc -> "cba"
+
+L.bindFunction:
+  sum -> echo(2+2)
+
+
 L.bindFunction("alphabet", abc)
+#L.echo("Hello world")
+echo L.dostring("mylib.abc()")
